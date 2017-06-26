3f3fa475b19fc094fd82fd98d63d91a224a06edf
Abhinandan Panigrahi <chrzrdx@gmail.com>
2017-06-26 14:48:38 +0530

    Add axios and bind it to this.$http

--- template/src/main.js
+++ template/src/main.js
@@ -2,17 +2,21 @@ require(`./themes/app.${__THEME}.styl`)
 
 import Vue from 'vue'
 import Quasar from 'quasar'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
-import { sync } from 'vuex-router-sync'
-
-sync(store, router) // vuex-router-sync
-
-Vue.config.productionTip = false
-Vue.use(Quasar)
 
 import 'quasar-extras/fontawesome'
 // import { } from 'quasar-extras/animate'
+
+sync(store, router) // sync router with vuex store
+
+Vue.use(VueAxios, axios) // bind axios to this.$http
+Vue.use(Quasar)
+
+Vue.config.productionTip = false
 
 Quasar.start(() => {
   /* eslint-disable no-new */
