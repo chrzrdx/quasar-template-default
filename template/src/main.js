f631098d396518b69a662032e228aef94ca354be
Abhinandan Panigrahi <chrzrdx@gmail.com>
2017-06-26 14:33:02 +0530

    Setup vuex store with demo module and sync router with store

--- template/src/main.js
+++ template/src/main.js
@@ -1,31 +1,25 @@
-// === DEFAULT / CUSTOM STYLE ===
-// WARNING! always comment out ONE of the two require() calls below.
-// 1. use next line to activate CUSTOM STYLE (./src/themes)
 require(`./themes/app.${__THEME}.styl`)
-// 2. or, use next line to activate DEFAULT QUASAR STYLE
-// require(`quasar/dist/quasar.${__THEME}.css`)
-// ==============================
 
 import Vue from 'vue'
 import Quasar from 'quasar'
 import router from './router'
+import store from './store'
+import { sync } from 'vuex-router-sync'
+
+sync(store, router) // vuex-router-sync
 
 Vue.config.productionTip = false
-Vue.use(Quasar) // Install Quasar Framework
+Vue.use(Quasar)
 
-if (__THEME === 'mat') {
-  // require('quasar-extras/roboto-font')
-}
-// import 'quasar-extras/material-icons'
-// import 'quasar-extras/ionicons'
 import 'quasar-extras/fontawesome'
-import 'quasar-extras/animate'
+// import { } from 'quasar-extras/animate'
 
 Quasar.start(() => {
   /* eslint-disable no-new */
   new Vue({
     el: '#q-app',
     router,
+    store,
     render: h => h(require('./App'))
   })
 })
